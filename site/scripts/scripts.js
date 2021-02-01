ecfac02e1a1dafa3a5d3ab107bd5b127f08fc1a7
Vinícius <mvlca@ecomp.poli.br>
2021-02-01 19:44:34 -0300

    site v1

---
+++ site/scripts/scripts.js
@@ -0,0 +1,47 @@
+function aparecerTextoFuncional() {
+    document.getElementById("js-card-funcional").innerHTML = "Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional. Aula sobre funcional.";
+}
+
+function resetFuncional() {
+    document.getElementById("js-card-funcional").innerHTML = "+SAIBA MAIS";
+}
+
+function aparecerTextoMusculacao() {
+    document.getElementById("js-card-musculacao").innerHTML = "Aula sobre musculação. Aula sobre musculação. Aula sobre musculação. Aula sobre musculação. Aula sobre musculação. Aula sobre musculação. Aula sobre musculação. Aula sobre musculação. Aula sobre musculação. Aula sobre musculação.";
+}
+
+function resetMusculacao() {
+    document.getElementById("js-card-musculacao").innerHTML = "+SAIBA MAIS";
+}
+
+function aparecerTextoCrosstraining() {
+    document.getElementById("js-card-crosstraining").innerHTML = "Aula sobre crosstraining. Aula sobre crosstraining. Aula sobre crosstraining. Aula sobre crosstraining. Aula sobre crosstraining. Aula sobre crosstraining. Aula sobre crosstraining.";
+}
+
+function resetCrosstraining() {
+    document.getElementById("js-card-crosstraining").innerHTML = "+SAIBA MAIS";
+}
+
+function aparecerTextoPilates() {
+    document.getElementById("js-card-pilates").innerHTML = "Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates. Aula sobre pilates.";
+}
+
+function resetPilates() {
+    document.getElementById("js-card-pilates").innerHTML = "+SAIBA MAIS";
+}
+
+function aparecerTextoDance() {
+    document.getElementById("js-card-dance").innerHTML = "Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance. Aula sobre dance.";
+}
+
+function resetDance() {
+    document.getElementById("js-card-dance").innerHTML = "+SAIBA MAIS";
+}
+
+function aparecerTextoBike() {
+    document.getElementById("js-card-bike").innerHTML = "Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. Aula sobre bike. ";
+}
+
+function resetBike() {
+    document.getElementById("js-card-bike").innerHTML = "+SAIBA MAIS";
+}
